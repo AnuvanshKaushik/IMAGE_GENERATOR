900f1252e25323eb2cc62bb75b12a85ed27f1329
Anuvansh <157476783+AnuvanshKaushik@users.noreply.github.com>
2025-06-21 12:51:53 +0530

    Update index.js

--- Client/src/apis/index.js
+++ Client/src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://image-generator-backend-q8dwj3mh7.vercel.app/api/",
 });
 
 export const GetPosts = async () => await API.get("post/");
